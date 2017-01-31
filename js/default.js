2de22df8f80220f1046e85159373441a39ca31f2
Griffin Wiebel <griffinwiebel@gmail.com>
2017-01-31 15:49:02 -0700

    Reduced image size, added animations, and more fix

--- js/default.js
+++ js/default.js
@@ -1,4 +1,5 @@
-$(function() {
+$( document ).ready(function() {
+
 	$('a[href*="#"]:not([href="#"])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
@@ -10,6 +11,36 @@ $(function() {
 			}
 		}
 	});
-	
+
+	/* Detecting Elements in View */
+	var $animation_elements = $('.animation-element');
+	var $window = $(window);
+
+	function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top+200;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      $element.removeClass('in-view');
+    }
+  });
+}
+
+
+$window.on ('scroll resize', check_if_in_view);
+$window.trigger('scroll');
+
+	// Initiate Rellax
 	var rellax = new Rellax('.rellax');
 });
