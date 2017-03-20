0c2611cd415d661609c478adb0cc3fe9b8eb5139
Griffin Wiebel <Griffin Wiebel>
2017-03-20 15:54:48 -0700

    Added instagram feed and work carousel.
    Prepped future update with more images.

--- js/default.js
+++ js/default.js
@@ -1,12 +1,12 @@
-$( document ).ready(function() {
+$(document).ready(function () {
 
-	$('a[href*="#"]:not([href="#"])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	$('a[href*="#"]:not([href="#"])').click(function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			if (target.length) {
 				$('html, body').animate({
 					scrollTop: target.offset().top
-					}, 1000);
+				}, 1000);
 				return false;
 			}
 		}
@@ -17,28 +17,53 @@ $( document ).ready(function() {
 	var $window = $(window);
 
 	function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
+		var window_height = $window.height();
+		var window_top_position = $window.scrollTop();
+		var window_bottom_position = (window_top_position + window_height);
 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top+200;
-    var element_bottom_position = (element_top_position + element_height);
+		$.each($animation_elements, function () {
+			var $element = $(this);
+			var element_height = $element.outerHeight();
+			var element_top_position = $element.offset().top + 200;
+			var element_bottom_position = (element_top_position + element_height);
 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-        (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    }
-  });
-}
+			//check to see if this current container is within viewport
+			if ((element_bottom_position >= window_top_position) &&
+				(element_top_position <= window_bottom_position)) {
+				$element.addClass('in-view');
+			}
+		});
+	}
 
 
-$window.on ('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');
 
 	// Initiate Rellax
 	var rellax = new Rellax('.rellax');
+
+	// Initialize instafeed
+	var feed = new Instafeed({
+		get: 'user',
+		userId: '400537280',
+		limit: '5',
+		clientId: '9733e03825fb4aada5d65dcfe740baa3',
+		accessToken: '400537280.ba4c844.a4b7763f74a0434ab98d9ae737445385',
+		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
+	});
+	feed.run();
+
+	// Initialize flickity
+	$('.work-carousel').flickity({
+		cellAlign: 'left',
+		wrapAround: true,
+		freeScroll: true,
+		pageDots: false,
+		resize: true,
+		setGalleryHeight: false
+	});
+
+	// $('.carousel-cell').hover(function() {
+	// 	$('work_hover_text').html("<p>" + ( $(this).data('site') ) + "</p>") ;
+	// });
 });
