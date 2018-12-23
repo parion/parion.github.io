a2905b5394f8992f8f40403c044ed7acd75b4f73
Griffin Wiebel <griffinwiebel@gmail.com>
2018-12-22 22:47:57 -0700

    Portfolio v2.0.0 First big update to the portfolio, including a blog. Much work ahead.

---
+++ src/pages/portfolio.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class PortfolioIndex extends React.Component {
+  render() {
+    return (
+      <div className="intro"
+        style={{
+          textAlign: `center`,
+        }}>
+        <h4>Hi, I'm Griffin</h4>
+        <p>I'm a software developer at General Motors and an Arizona State University alumni.</p>
+        <p>🕐 Portfolio redesign coming soon! For now, check out my <a href="/">blog</a>.</p>
+      </div>
+    )
+  }
+}
+
+export default PortfolioIndex;
